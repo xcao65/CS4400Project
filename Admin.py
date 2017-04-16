86a72935062ebc3c9791cd37555d455ac462ddf3
Xun <xcao65@gatech.edu>
2017-04-15 21:49:53 -0400

    admin

--- Admin.py
+++ Admin.py
@@ -1,7 +1,13 @@
 
 
 class Admin():
-	def PendingDataPoint(self, connection):
+	def pendingDataPoint(self, connection):
 		cursor = connection.cursor()
 		sql = "SELECT LocName AS POI location, DataType AS Data type, DataValue AS Data value, DateTime AS Time&date of data reading, FROM Data_Point WHERE Status = Pending"
 		cursor.execute(sql)
+	def pendingCityOfficialAccounts(self, connection):
+		cursor = connection.cursor()
+		sql = "SELECT UserName, EmailAddress AS Email, City, State, Title, FROM User, City_Official, WHERE Status = Pending"
+		cursor.execute(sql)
+
+	
