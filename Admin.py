8e276b10b0dee7b6ee48460549df9f89d6949604
Xun Cao <xcao65@gatech.edu>
2017-04-16 13:21:56 -0400

    Draft for changing data point

--- Admin.py
+++ Admin.py
@@ -1,11 +1,14 @@
 class Admin():
+
+	#fetch pending data point
 	def pendingDP(self, connection):
 		cursor = connection.cursor()
-		sql = "SELECT LocName, DataType, DataValue FROM Data_Point WHERE Status = %s"
+		sql = "SELECT LocName, DataType, DataValue, DateTime FROM Data_Point WHERE Status = %s"
 		cursor.execute(sql, 'Pending')
-		result = cursor.fetchone()
+		result = cursor.fetchall()
 		print result
 
+	#fetch pending City official accounts
 	def pendingCOA(self, connection):
 		cursor = connection.cursor()
 		sql = "SELECT UserName, B.EmailAddress, City, State, Title FROM User AS A, City_Official AS B WHERE Status = %s AND A.EmailAddress = B.EmailAddress"
@@ -13,3 +16,21 @@ class Admin():
 		result = cursor.fetchall()
 		print len(result)
 
+	#change status of data point
+	def changeDP(self, index, results, connection, bool):
+		if bool == 1:
+			status = 'Approved'
+		else:
+			status = 'Rejected'
+
+		result = []
+		cursor = connection.cursor()
+
+		for i in index:
+			sql = "UPDATE Data_Point SET Status = %s WHERE LocName = %s AND DateTime = %s"
+			key1 = (results[i])["LocName"]
+			key2 = (results[i])["DateTime"]
+			cursor.execute(sql, status, key1, key2)
+			result.add(cursor.fetchone())
+
+		print result
