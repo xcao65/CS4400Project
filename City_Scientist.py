cb015ad5a81d86724da7ff065bcaa443b6e3a813
F0rge1cE <xuxueyang1994@qq.com>
2017-04-15 22:42:55 -0400

    Add city scientist

---
+++ City_Scientist.py
@@ -0,0 +1,84 @@
+from datetime import datetime
+
+################
+# Test only
+import pymysql
+import pymysql.cursors
+
+class CityScientist():
+
+	# Allow city scientists to add new location to the POI table.
+	def addNewLocation(self, locationname, city, state, zipcode, connection):
+		try:
+			with connection.cursor() as cursor:
+				# Construct the SQL expression				
+				sql = "INSERT INTO POI (Flag, LocationName, ZipCode, City, State) VALUES (%s, %s, %s, %s, %s)"
+
+				# MySQL automatically set False = 0, True = 1. So explicitly change the first variable
+				init_data = (0, locationname, zipcode, city, state)
+				cursor.execute(sql, init_data);
+
+				# Get the response from DB
+				result = cursor.fetchone()
+				if result is not None:
+					raise Exception("Bad response in addNewLocation - CityScientist!", result)
+				else:
+					connection.commit()
+		except ExceptionType, Argument:
+				print "Type:%s , %s  in addNewLocation of CityScientist!" % (ExceptionType, Argument)
+		finally:
+			if connection:
+				connection.close()
+
+
+	# Allow city scientists to
+	def addNewDataPoint(self, locationname, date, time, datatype, value, connection):
+		try:
+			with connection.cursor() as cursor:
+				# Construct the SQL expression. Use default value for Status (e.g. "Pending").			
+				sql = "INSERT INTO Data_Point (DateTime, LocName, DataType, DataValue) VALUES (%s, %s, %s, %s)"
+
+				# Assume time format: Date: yyyy/mm/dd ; Time: hh:mm AS STRING
+				DateTime = date.split('/') + time.split(':')
+				DateTime = map(int, DateTime)
+				# Convert the format into yyyy-mm-dd hh:mm
+				Formalized_DateTime = datetime(*DateTime).strftime('%Y-%m-%d %H:%M')
+
+				init_data = (Formalized_DateTime, locationname, datatype, value)
+				cursor.execute(sql, init_data);
+
+				# Get the response from DB
+				result = cursor.fetchone()
+				if result is not None:
+					raise Exception("Bad response in addNewDataPoint of CityScientist!", result)
+				else:
+					connection.commit()
+				connection.commit()
+		except ExceptionType, Argument:
+				print "Type:%s , %s  in addNewDataPoint of CityScientist!" % (ExceptionType, Argument)
+		finally:
+			if connection:
+				connection.close()
+
+
+################
+# Test only
+connection = pymysql.connect(host='academic-mysql.cc.gatech.edu',
+                             user='cs4400_Group_79',
+                             password='tpyJ6PVQ',
+                             db='cs4400_Group_79',
+                             # charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+scientistTest = CityScientist()
+# scientistTest.addNewLocation("ddd", "Atlanta", "Georgia", "30318", connection)
+scientistTest.addNewDataPoint("ccc", "1999/03/04", "05:06", "Mold", 666, connection)
+
+
+
+
+
+
+
+
+
