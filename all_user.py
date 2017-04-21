61f0852770af9b528651993d25868776674657ba
Jing Gu <jgu47@gatech.edu>
2017-04-21 15:32:08 -0400

    [+] Change Log_in API and Add connection function

--- all_user.py
+++ all_user.py
@@ -1,39 +1,46 @@
-# from connection import Connection
+from connection import *
 
 class LogIn():
-    def login(self, name, pwd, connection):
+    def login(self, name, pwd):
         username = name
         password = pwd
+        connection = connect()
         cursor = connection.cursor()
         sql = "SELECT Username, Password, Type FROM User WHERE Username = %s"
         cursor.execute(sql, username)
         results = cursor.fetchone()
+        connection.close()
 
         if username == "" or password == "":
             print("Error! Please do not leave any fields blank")
+            return None
         else:
             if results is None:
-                print("Invalid! Please enter a correct username and password combination")
+                print "Invalid! Please enter a correct username and password combination"
+                return None
             else:
                 if password == results["Password"]:
-                    print results
-                    print("Congrats! You successfully logged in")
+                    # print results
                     utype = results["Type"]
-                    print utype
+                    print("Congrats! You successfully logged in")
+                    #print utype
+                    return utype
                 else:
-                    print("Error! Please enter a correct username and password combination")
+                    print "Error! Please enter a correct username and password combination"
+                    return None
 
 
-    def register(self, name, email, pwd, cpwd, utype, connection, *others):
-        if not self.checkUniqueName(name, connection):
+    def register(self, name, email, pwd, cpwd, utype, *others):
+        if not self.checkUniqueName(name):
             print 'Username has been used, try another one'
             return
-        if not self.checkUniqueEmail(email, connection):
+        if not self.checkUniqueEmail(email):
             print 'Email has been registered, try another one'
             return
         if pwd != cpwd:
             print 'password must be matched'
             return
+        connection = connect()
         cursor = connection.cursor()
         sql = "INSERT INTO User (EmailAddress, UserName, Password, Type) VALUES (%s, %s, %s, %s)"
         cursor.execute(sql, (email, name, pwd, utype))
@@ -48,17 +55,39 @@ class LogIn():
             cursor.execute(sql, (email, extras[0], extras[1], extras[2]))
             connection.commit()
             print 'Added New City Official Successfully'
+        connection.close()
 
-    def checkUniqueName(self, name, connection):
+    def checkUniqueName(self, name):
+        connection = connect()
         cursor = connection.cursor()
         sql = "SELECT username FROM User WHERE Username = %s"
         cursor.execute(sql, name)
         results = cursor.fetchone()
+        connection.close()
         return results == None
 
-    def checkUniqueEmail(self, email, connection):
+    def checkUniqueEmail(self, email):
+        connection = connect()
         cursor = connection.cursor()
         sql = "SELECT EmailAddress FROM User WHERE EmailAddress = %s"
         cursor.execute(sql, email)
         results = cursor.fetchone()
+        connection.close()
         return results == None
+
+    def deleteUser(self, name):
+        connection = connect()
+        cursor = connection.cursor()
+        sql = "DELETE FROM User WHERE Username =  %s "
+        # cursor.execute(sql, 'Justin Bieber')
+        cursor.execute(sql, name)
+        connection.commit()
+        connection.close()
+        print 'delete successfully'
+
+if __name__ == "__main__":
+    test = LogIn()
+    # print test.register('Oprah Winfrey','Oprah.Winfrey@gatech.edu', 'OprahWinfrey','OprahWinfrey', 'City Official', 'Major', 'Jacksonville', 'Florida')
+    # print test.checkUniqueName('Justin Bieber')
+    print test.login('Oprah Winfrey', 'OprahWinfrey')
+    # print test.deleteUser('Oprah Winfrey')
