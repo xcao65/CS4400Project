bbbe8e0919ce8a0a8036ac3d0dc9b7c1b4c3c9a9
Jing Gu <jgu47@gatech.edu>
2017-04-21 00:40:26 -0400

    [*] Refactor for scientist home.

--- static/js/scientist.js
+++ static/js/scientist.js
@@ -1,17 +1,23 @@
 angular.module('p2', ['ngRoute'])
   .config(function ($routeProvider) {
-     $routeProvider
-        .when('/locations', {
-           templateUrl: '/locations.html',
-           controller: 'LocationListCtrl',
-           controllerAs: 'locations'
+     $routeProvider.when('/add_loc', {
+          'templateUrl': '/add_location.html'
+          , 'controller': 'AddLocationCtrl'
+          , 'controllerAs': 'add_loc'
+        }).when('/add_point', {
+          'templateUrl': '/add_point.html'
+          , 'controller': 'AddDataPointCtrl'
+          , 'controllerAs': 'add_point'
         })
         .otherwise({
-           redirectTo: '/locations'
+           redirectTo: '/add_point'
         });
-  }).factory('LocationsFactory', function LocationsFactory ($http) {
+  }).factory('Commons', function Commons ($http, $location) {
      var exports = {};
-
+     
+     exports.goto = function(view) {
+       $location.path(view); // path not hash
+     }
      exports.get_types = function () {
         return $http.get('api/types')
            .error(function (data) {
@@ -37,42 +43,40 @@ angular.module('p2', ['ngRoute'])
         })
      }
      return exports
-  }).controller('LocationListCtrl', function($scope, LocationsFactory) {
-     $scope.p = {'ts': "2017-03-19T16:00"}
-     $scope.l = {'city': 0, 'state': 0}
-     $scope.m = true // true for adding point, false for adding location
-     $scope.switch = function() {
-       this.m = !this.m
-     }
-     
-     $scope.states = [{"id": 0, "name": "Georgia"}, {"id": 1, "name": "Alaska"}]
-     $scope.cities = [{"id": 0, "name":"Atlanta"}, {"id": 1, "name":"Smyrna"}]
-     $scope.save_point = function() {
-       LocationsFactory.put_point(this.p)
-        .success(function(data, status) {
-          console.log("Successfully saved point: ", status, data)
-        })
-     }
-     $scope.save_loc = function() {
-       LocationsFactory.put_loc(this.l).success(function(data, status) {
-         console.log("Successfully saved location: ", status, data)
-         $scope.locations.push(data.c)
-       })
-     }
-     LocationsFactory.get_types()
-        .success(function(data, statusCode) {
-           console.log('Successfully got types!', statusCode, data)
-          if(data.succ == 0) {
-            $scope.types = data.c
-            $scope.p.attr = data.c[0].id
-          }
-     })
-     LocationsFactory.get_locations()
-        .success(function(data, statusCode) {
-          console.log('Successfuly got locations!', statusCode, data)
-          if(data.succ == 0) {
-            $scope.locations = data.c
-            $scope.p.loc = data.c[0].id
-          }
-     })
+  }).controller('AddLocationCtrl', function($scope, Commons) {
+    $scope.l = {'city': 0, 'state': 0}
+    $scope.states = [{"id": 0, "name": "Georgia"}, {"id": 1, "name": "Alaska"}]
+    $scope.cities = [{"id": 0, "name":"Atlanta"}, {"id": 1, "name":"Smyrna"}]
+    
+    $scope.save_loc = function() {
+      Commons.put_loc(this.l).success(function(data, status) {
+        console.log("Successfully saved location: ", status, data)
+        if(data.succ == 0) alert("Success!")
+      })
+    }
+  }).controller('NavCtrl', function($scope, Commons) {
+    $scope.goto = Commons.goto
+  }).controller('AddDataPointCtrl', function($scope, Commons) {
+    $scope.goto = Commons.goto
+    $scope.p = {'ts': "2017-03-19T16:00"}
+    
+    Commons.get_types().success(function(data, statusCode) {
+      console.log('Successfully got types!', statusCode, data)
+      if(data.succ != 0) return
+      $scope.types = data.c
+      $scope.p.attr = data.c[0].id
+    })
+    Commons.get_locations().success(function(data, statusCode) {
+      console.log('Successfuly got locations!', statusCode, data)
+      if(data.succ != 0) return
+      $scope.locations = data.c
+      $scope.p.loc = data.c[0].id
+    })
+    
+    $scope.save_point = function() {
+      Commons.put_point(this.p).success(function(data, status) {
+        console.log("Successfully saved point: ", status, data)
+        if(data.succ == 0) alert("Success!")
+      })
+    }
   })
